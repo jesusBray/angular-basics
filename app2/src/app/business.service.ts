b4db4a6f551f2051f1e424ef35b74b0df434bbfd
Jesus Br P <jesus.brayan.paye@gmail.com>
2019-04-11 09:34:31 -0400

    generate login user

---
+++ app2/src/app/business.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusinessService {
+
+  uri = 'http://localhost:4000/business';
+
+  constructor(private http: HttpClient) { }
+
+  addBusiness(person_name, business_name, business_gst_number) {
+    const obj = {
+      person_name: person_name,
+      business_name: business_name,
+      business_gst_number: business_gst_number
+    };
+    console.log(obj);
+    this.http.post(`${this.uri}/add`, obj)
+        .subscribe(res => console.log('Done'));
+  }
+}
